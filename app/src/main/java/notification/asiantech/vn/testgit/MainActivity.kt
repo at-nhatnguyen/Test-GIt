3317bb8dcadded2a4a1593d00b1219e8dbe350dd
at-nhatnguyen <nhat.nguyen@asiantech.vn>
2020-02-13 09:47:20 +0700

    first commit

---
+++ app/src/main/java/notification/asiantech/vn/testgit/MainActivity.kt
@@ -0,0 +1,12 @@
+package notification.asiantech.vn.testgit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
